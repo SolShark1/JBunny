1dc4d63e7853e9f9c3649f27e683e2b9f90a28ab
SolShark1 <tattooed112@yahoo.com>
2024-08-02 16:06:37 -0400

    Update JBunny.py

---
+++ JBunny.py
@@ -0,0 +1,2 @@
+pip install tweepy 
+NAcKJqfC15RvAKT5JCtna5tUi, J4K2JWV86wc30ZuqIx0xWlbZGEM4ivQdgis8Y9Ghimjf2KMoTW,1806829238198079488-xEhCjXK0pd9M2IGSd5eIaYXdpnNjZi, 7252679351:AAEsiNJJ4zfskJIABiRIlomE2QBaq-cMXj0,jbunnyonsol, jbunnyonsol
